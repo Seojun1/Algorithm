3e0206fc9c898466aede16f21b898eae8aedeaef
LeeSeoJun <77569680+Seojun1@users.noreply.github.com>
2022-03-15 22:49:53 +0900

    .

--- 백준/1차원 배열/07.py
+++ 백준/1차원 배열/07.py
@@ -1,8 +1,19 @@
 N = int(input())
+
+# 환경구축
 all = []
 for i in range(N):
     a = list(map(int, input().split()))
-    b = a.pop(0)
-    print(sum(a) / b)
-    # 비율을 구하는 방법?
+    b = a.pop(0) # 임시의 학생 수는 따로 제외하기
+    c = (sum(a) / b) # 평균
     
+    # 탐색 시작
+    for l in a:
+        if (l > c):
+            all.append(l)
+        else:
+            pass
+    result = format((len(all) / b) * 100, ".3f") # 비율 구하는 공식
+    print(str(result) + '%')
+    # 초기화
+    all = []
