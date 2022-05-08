8aea98e53bc81906c1dd28502de2351e1d62a039
LeeSeoJun <SeoCoding123@gmail.com>
2022-05-08 15:01:34 +0900

    HA.....

---
+++ 백준/if문/07.py
@@ -0,0 +1,14 @@
+# 주사위 세개
+a, b, c = map(int, input().split())
+
+# 탐색 시작
+if (a == b == c):
+    print(10000 + (a * 1000))
+elif a == b:
+    print(1000 + a * 100)
+elif a == c:
+    print(1000 + a * 100)
+elif b == c:
+    print(1000 + b * 100)
+else:
+    print(max(a,b,c) * 100)
