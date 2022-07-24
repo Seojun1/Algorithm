70d60f124d5d11aebe2278bede06ac26fc5eda2f
LeeSeoJun <SeoCoding123@gmail.com>
2022-07-24 18:49:35 +0900

    finish

--- 백준/알고리즘 분류/정렬/1181.py
+++ 백준/알고리즘 분류/정렬/1181.py
@@ -1,15 +1,14 @@
-# 단어 정렬
-import sys
-N = int(sys.stdin.readline())
+N = int(input())
 a = []
 for i in range(N):
     a.append(input())
-arr = list(set(a))
 
-sort_arr = []
-for i in arr:
-    sort_arr.append((len(i), i))
-sort_arr = sorted(sort_arr)
+# 중복의 문자열을 없애기 위해 사용(합집합)
+a = list(set(a))
+result = []
+for j in a:
+    result.append([len(j), j])
 
-for j in sort_arr:
-    print(j[1])
+result = sorted(result)
+for i in range(len(result)):
+    print(result[i][1])
--- 백준/알고리즘 분류/정렬/1427.py
+++ 백준/알고리즘 분류/정렬/1427.py
@@ -1,6 +1,6 @@
 # 소트인사이드
-N = list(map(int, input()))
-N.sort()
-N.reverse()
-for i in N:
-    print(i, end='')
+import sys
+N = sys.stdin.readline()
+N = sorted(N)
+for i in range(len(N) -1 , 0-1, -1):
+    print(N[i], end='')
